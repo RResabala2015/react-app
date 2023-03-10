417ef5db0bc3fbc0167af66c10d28a33cf771d6d
Renato Resabala <renatoresabala@gmail.com>
2023-03-09 20:13:24 -0500

    feature: first module of master class
     - Components, css, js, props.
     - Two way binding.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Expenses from './components/Expenses/Expenses';
+
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 1, 14),
+    },
+    { id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2023, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 3, 14),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk',
+      amount: 450,
+      date: new Date(2023, 4, 14),
+    },
+  ];
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get Started!"),
+    React.createElement(Expenses,{items: expenses}),
   );
+
 }
 
 export default App;
